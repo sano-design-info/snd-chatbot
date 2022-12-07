fa3025dcfe2037b0c214802d3d5c24d9fe77edb8
hrsano645 <hrs.sano645@gmail.com>
2022-12-07 16:45:43 +0900

    add save imgs, attachmentfiles

--- print_mail/test_gmail_api.py
+++ print_mail/test_gmail_api.py
@@ -70,15 +70,22 @@ def main():
         )
         # 一番上のものでいいから表示
         threads = thread_results.get("threads", [])
-        # print(threads)
+        from pprint import pprint
 
-        top_message_id = threads[0].get("id", "")
+        pprint(threads)
+
+        # 上位10件のスレッド -> メッセージを取得。見てもスレッド数が2件ぐらいのもので十分かな
+        # 上位10のスレッドから > メッセージの最初取り出して、その中から選ぶ
+        # 選択後のメッセージを元に処理開始
+
+        top_message_id = threads[2].get("id", "")
+        print(top_message_id)
 
         message_result = (
             service.users().messages().get(userId=target_userid, id=top_message_id)
         ).execute()
 
-        message_payload = message_result.get("payload", {})
+        message_payload = message_result.get("payload")
         message_headers = message_payload.get("headers")
 
     except HttpError as error:
@@ -110,28 +117,45 @@ def main():
         f"title:{message_title}\nfrom:{message_from_addresss}\ncc:{message_cc_address}\n{message_datetime}"
     )
 
+    # メールのmimeマルチパートを考慮して、構造が違うモノに対応する
+    # relative> altanative の手順で掘る。mixedは一番上で考慮しているのでここでは行わない
+    message_body_related = {}
     message_body_parts = next(
         (
             i
             for i in message_payload.get("parts")
             if i.get("mimeType") == "multipart/alternative"
+        ),
+        {},
+    ).get("parts", [])
+    if not message_body_parts:
+        message_body_related = next(
+            (
+                i
+                for i in message_payload.get("parts")
+                if i.get("mimeType") == "multipart/related"
+            )
         )
-    ).get("parts")
+        message_body_parts = next(
+            (
+                i
+                for i in message_body_related.get("parts")
+                if i.get("mimeType") == "multipart/alternative"
+            )
+        ).get("parts")
 
     messages_text_parts = [i for i in message_body_parts if "text" in i.get("mimeType")]
 
     b64dec_msg_byte = decode_base64url(messages_text_parts[1].get("body").get("data"))
 
-    # print("decode msg")
-    # print(b64dec_msg_byte)
-
-    mail_html_bs4 = BeautifulSoup(b64dec_msg_byte)
-    only_body = mail_html_bs4.body()
     # imgタグを除去する
 
-    # print(only_body[0])
-    # jinja2埋込
+    mail_html_bs4 = BeautifulSoup(b64dec_msg_byte, "html.parser")
+    only_body_tags = mail_html_bs4.body
+    for t in only_body_tags.find_all("img"):
+        t.decompose()
 
+    # jinja2埋込
     # テンプレート読み込み
     env = Environment(
         loader=FileSystemLoader(str(Path(__file__).absolute().parent), encoding="utf8")
@@ -140,7 +164,7 @@ def main():
 
     # 設定ファイル読み込み
     params = {
-        "export_html": only_body[0],
+        "export_html": only_body_tags,
         "message_title": message_title,
         "message_from_addresss": message_from_addresss,
         "message_cc_address": message_cc_address,
@@ -152,6 +176,54 @@ def main():
 
     with Path("./export_mail.html").open("w", encoding="utf8") as exp_mail_hmtl:
         exp_mail_hmtl.write(rendered_html)
+
+    # メール本文にimgファイルがある場合はそれを取り出す
+    # multipart/relatedの時にあるので、それを狙い撃ちで取る
+
+    def save_attachment_file(
+        service, filename: str, message_id: str, attachment_id: str
+    ) -> None:
+        attachfile_data = (
+            service.users()
+            .messages()
+            .attachments()
+            .get(
+                userId=target_userid,
+                messageId=message_id,
+                id=attachment_id,
+            )
+            .execute()
+        )
+        # print(attachfile_data)
+        with Path(filename).open("wb") as msg_img_file:
+            msg_img_file.write(decode_base64url(attachfile_data.get("data")))
+
+    if message_body_related:
+        message_imgs = [
+            i for i in message_body_related.get("parts") if "image" in i.get("mimeType")
+        ]
+        print(message_imgs)
+        for msg_img in message_imgs:
+            # print(msg_img.get("filename"))
+            save_attachment_file(
+                service,
+                msg_img.get("filename"),
+                top_message_id,
+                msg_img.get("body").get("attachmentId"),
+            )
+    # 添付ファイルの保持
+
+    message_attachmentfiles = [
+        i for i in message_payload.get("parts") if "application" in i.get("mimeType")
+    ]
+    print(message_attachmentfiles)
+    for msg_attach in message_attachmentfiles:
+        save_attachment_file(
+            service,
+            msg_attach.get("filename"),
+            top_message_id,
+            msg_attach.get("body").get("attachmentId"),
+        )
     exit()
 
 
