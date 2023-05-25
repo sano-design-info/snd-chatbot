72084714ae2bbc90007d37da7ca5f78ed1936d69
hrsano645 <hrs.sano645@gmail.com>
2023-05-25 13:56:32 +0900

    add gmailapi test(only mail create)

--- tests/test_api/test_gmailapi.py
+++ tests/test_api/test_gmailapi.py
@@ -1,6 +1,5 @@
 import base64
-import mimetypes
-from email.message import EmailMessage
+import email
 
 import pytest
 
@@ -21,11 +20,18 @@ def test_create_messagedata():
         "attachment_files": None,
     }
 
-    # テストデータを元にメッセージを作成
+    # テストデータを元にメッセージを作成 (base64エンコードされたバイナリ)
     result_b64safebinary = create_messagedata(**testdata)
 
-    # メッセージをデコードして、メッセージオブジェクトにする
-    result_message = EmailMessage.from_bytes(result_b64safebinary)
+    # base64でエンコードされたメッセージをMessageオブジェクトに変換
+    result_message = email.message_from_bytes(
+        base64.urlsafe_b64decode(result_b64safebinary)
+    )
 
     # メッセージオブジェクトの検証
     assert result_message is not None
+    assert result_message["From"] == testdata["sender"]
+    assert result_message["To"] == testdata["to"]
+    assert result_message["Cc"] == testdata["cc"]
+    assert result_message["Reply-To"] == testdata["reply_to"]
+    assert result_message["Subject"] == testdata["subject"]
