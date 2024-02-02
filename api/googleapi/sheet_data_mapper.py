b8a8454216fe233eb2884fa15209d02ebe188393
hrsano645 <hrs.sano645@gmail.com>
2024-02-02 10:18:25 +0900

    change: sheetname use config

--- api/googleapi/sheet_data_mapper.py
+++ api/googleapi/sheet_data_mapper.py
@@ -2,27 +2,24 @@ from googleapiclient.discovery import Resource
 from googleapiclient.errors import HttpError
 
 
-def write_data_to_sheet(
-    gsheet_service: Resource, spreadsheet_id, sheet_name, data, cell_mapping
-):
+def write_data_to_sheet(gsheet_service: Resource, spreadsheet_id, data, cell_mapping):
+    single_updates, table_updates = [], []
+
     # 単一セルへのデータ書き込み
-    single_updates = []
-    for key, cell in cell_mapping["singlecell"].items():
+    for key, cell in cell_mapping.get("singlecell", {}).items():
         value = data.get(key, "")
-        single_updates.append({"range": f"{sheet_name}!{cell}", "values": [[value]]})
+        single_updates.append({"range": cell, "values": [[value]]})
 
     # テーブルデータへの書き込み
-    table_updates = []
-    for table_name, table_info in cell_mapping["tables"].items():
-        items = data[table_name]
+    for table_name, table_info in cell_mapping.get("tables", {}).items():
+        items = data.get(table_name, [])
         start_row = table_info["startRow"]
         for i, item in enumerate(items):
             row = start_row + i
             for column_name, column_letter in table_info["columns"].items():
                 value = item.get(column_name, "")
-                table_updates.append(
-                    {"range": f"{sheet_name}!{column_letter}{row}", "values": [[value]]}
-                )
+                cell_address = f"{column_letter}{row}"
+                table_updates.append({"range": cell_address, "values": [[value]]})
 
     # 全ての更新をバッチで適用
     body = {"valueInputOption": "USER_ENTERED", "data": single_updates + table_updates}
